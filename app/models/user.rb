e039a23c3e922f5e6cbf5a8a9f21f6a0b31ced68
Thi-Ponce <thi.pponce@gmail.com>
2022-06-30 19:06:02 -0300

    create models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
+  has_many :categories
+  has_many :entities, foreign_key: :user_id
+
+  validates :name, presence: { message: "Enter a valid name" }
 end
