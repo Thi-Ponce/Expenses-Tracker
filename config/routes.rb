24faefe156fbac40b703411f984d4219a185befe
Thi-Ponce <thi.pponce@gmail.com>
2022-06-30 19:10:30 -0300

    Database and routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  resources :entity
+  get 'entity/index'
+  get 'entity/show'
+  get 'entity/new'
+  post '/entity/new', to: "entity#create", as: 'entity_create'
   devise_for :users
   get 'splash/show'
   root 'splash#show'
+  get 'categories/index'
+  get 'categories/new'
+  resources :categories
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
