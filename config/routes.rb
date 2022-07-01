d7af546fcd21a2431c54f01b279921a7c084eeaf
Thi-Ponce <thi.pponce@gmail.com>
2022-07-01 13:41:53 -0300

    required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  resources :entity
-  get 'entity/index'
-  get 'entity/show'
-  get 'entity/new'
+  resources :categories, only: [:index, :show]
+  resources :entities, only: [:index, :show]
   post '/entity/new', to: "entity#create", as: 'entity_create'
   devise_for :users
   get 'splash/show'
   root 'splash#show'
-  get 'categories/index'
-  get 'categories/new'
-  resources :categories
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
