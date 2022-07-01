802c86dcfbcc12c7140d582aeed994c883611f93
Thi-Ponce <thi.pponce@gmail.com>
2022-06-30 23:53:06 -0300

    install bullet and create model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user = User.new(name: 'Test', email: 'test@test', password: '123456')
+    @category = Category.new(user: @user, name: 'testing')
+  end
+
+  it 'Saves new category' do
+    category = Category.new(user: @user, name: 'testing')
+    expect(category).to be_valid
+  end
+
+  it 'Be invalid due to wrong name' do
+    category = Category.new(user: @user, name: '')
+    expect(category).to be_invalid
+  end
 end
--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user = User.new(name: 'Test', email: 'test@test', password: '123456')
+    @category = Category.new(user: @user, name: 'testing')
+  end
+
+  it 'Save new transaction' do
+    transaction = Entity.new(user: @user, category: @category, name: 'transaction1', amount: 10)
+    expect(transaction).to be_valid
+  end
+
+  it 'Be invalid due to wrong name' do
+    transaction = Entity.new(user: @user, category: @category, name: '', amount: 10)
+    expect(transaction).to be_invalid
+  end
+
+  it 'Be invalid due to wrong amount' do
+    transaction = Entity.new(user: @user, category: @category, name: 'transaction1', amount: 0)
+    expect(transaction).to be_invalid
+  end
 end
